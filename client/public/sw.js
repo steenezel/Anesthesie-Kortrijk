a53398de2191608283eb4d5e6c8fde6b08a61fcb
steenezel <pjsteelant@gmail.com>
2026-01-31 08:40:43 +0100

    manifest, sw, icons

---
+++ client/public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'anesthesie-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Installeren en opslaan van basisbestanden
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Slimme strategie: toon cache, maar update op de achtergrond
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
