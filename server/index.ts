527f37b99fadebd8e6173eeb7b1d5cfb725e88ff
steenezel <pjsteelant@gmail.com>
2026-02-18 16:33:21 +0100

    static.js

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes.js";
-import { serveStatic } from "./static";
+import { serveStatic } from "./static.js";
 import { createServer } from "http";
 
 const app = express();
