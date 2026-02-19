8064ee397e989dbd3dc1d697c9e232668c227698
steenezel <pjsteelant@gmail.com>
2026-02-19 07:59:36 +0100

    na analyse van de meeste code: high score?

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes.js";
-import { serveStatic } from "./static.js";
+import { registerRoutes } from "./routes";
+import { serveStatic } from "./static";
 import { createServer } from "http";
 
 const app = express();
