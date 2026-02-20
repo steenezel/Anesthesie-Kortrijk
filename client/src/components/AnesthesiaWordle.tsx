673c92552a23a481aef9b043e2240be559781360
steenezel <pjsteelant@gmail.com>
2026-02-20 19:22:00 +0100

    // @ts

--- client/src/components/AnesthesiaWordle.tsx
+++ client/src/components/AnesthesiaWordle.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// @ts-ignore
+// @ts-expect-error
 import { validWords } from '../data/puzzle_words_5.js';
 
 // --- JOUW WINNENDE WOORDEN LIJST ---
