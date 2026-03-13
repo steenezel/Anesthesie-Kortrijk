d7ff71fafe8c75118caef0ad8608b748daa31bf5
steenezel <pjsteelant@gmail.com>
2026-03-13 14:24:59 +0100

    Keys naar .env ipv publiek

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://asedlgeibrfbicbrmpkb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzZWRsZ2VpYnJmYmljYnJtcGtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzOTg4MDQsImV4cCI6MjA4ODk3NDgwNH0.nICr_Q9IlQO8ks9r65cs0Z34hmIjGCRhkYULlVjOmWk';
+const supabaseUrl = VITE_SUPABASE_URL;
+const supabaseAnonKey = VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
